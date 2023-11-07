63f85037a9515af06a484e553507614383420b13
enoch940508 <146065382+enoch940508@users.noreply.github.com>
2023-11-07 12:19:30 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main()
+{
+    int t, m;
+    double w;
+    scanf("%d%d",&t,&m);
+    if(t<=60)
+    {
+        w=t*m;
+
+    }
+    else if (61<=t&&t<=120)
+    {
+
+        w=60*m+(t-60)*m*1.33;
+
+    }
+    else if (t>121)
+    {
+
+        w=60*m+60*m*1.33+(t-120)*m*1.66;
+
+    }
+
+    printf("%.1f",w);
+}
